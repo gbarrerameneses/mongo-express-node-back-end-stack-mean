d783fd2c6bb5182ab8376264caeba1d9914d1d3a
Gustavo Barrera <gustavo.8805@gmail.com>
2024-07-03 15:36:26 -0600

    configurando servidor

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Desde index.js');
+const express = require('express');
+
+// Creando el servidor
+const app = express()
+
+const port = 4000
+
+// Definiendo ruta principal
+app.get('/', (req, res) => {
+    res.send('Hello World!') // enviando datos a pantalla
+  })
+
+app.listen(port, () => {
+    console.log('Servidor arriba');
+})
+
+
+
+
