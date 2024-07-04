931907840f915d456aac21d5d2237b5cfe21d4cd
Gustavo Barrera <gustavo.8805@gmail.com>
2024-07-04 16:41:13 -0600

    creación del producto, método POST success, se envia objeto a la BD

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,7 +1,20 @@
+const Producto = require("../models/Producto");
 
-
-
-exports.crearProducto = (req, res) => {
+exports.crearProducto = async (req, res) => {
     console.log('Desde controller crear producto');
     console.log(req.body);
+
+    try {
+        let producto;
+
+        // Creamos nuestro producto
+        producto = new Producto(req.body); // pasamos los parametros (objeto  ) de model Producto
+
+        await producto.save(); // almacenando el producto
+        res.send(producto); // devolvemos el mensaje con los datos ya guardados
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
